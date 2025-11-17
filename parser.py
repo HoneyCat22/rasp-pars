ddb3b832e178cfd3cf7b0f789f9cd7fef63bbc6a
HoneyCat22 <nm24032006@mail.ru>
2025-11-17 17:45:13 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,36 @@
+import requests
+
+def get_schedule_html(week_number=12, group='15.22д-э01/25б'):
+
+    url = "https://rasp.rea.ru/Schedule/ScheduleCard"
+    my_params = {
+    'selection': group,
+    'weekNum': week_number,
+    'catfilter': 0
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:144.0) Gecko/20100101 Firefox/144.0',
+        'Referer': 'https://rasp.rea.ru/?q=15.22%D0%B4-%D1%8D01%2F25%D0%B1',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept': 'application/json, text/javascript, */*; q=0.01'
+    }
+
+    session = requests.Session()
+
+    try:
+
+        response = session.get(url, params=my_params, headers=headers)
+        response.raise_for_status() 
+        print("Успех! Сервер ответил HTML-кодом.")
+        html_data = response.text
+        
+        # Код, чтобы сохранить html файл страницы 
+        # with open("schedule.html", "w", encoding="utf-8") as f:
+        #     f.write(response.text)
+        
+        return html_data, session, headers, my_params['selection']
+    
+    except requests.exceptions.RequestException as e: 
+            print(f"Ошибка при загрузке: {e}") 
+            return None, None, None, None 
